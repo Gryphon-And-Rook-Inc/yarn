9329ce71d63881c37f2652691e78d4b0ea49e5d0
gryphonandrook <38541934+gryphonandrook@users.noreply.github.com>
2022-11-15 22:48:25 -0700

    Update install.rb

--- recipes/install.rb
+++ recipes/install.rb
@@ -4,13 +4,25 @@
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
 execute 'update apt-get' do
-  command 'sudo apt-get update'
+  command 'sudo apt update'
+end
+
+execute 'upgrade apt-get' do
+  command 'sudo apt upgrade'
 end
 
 execute 'add yarn repo' do
   command 'curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -'
 end
 
+execute 'enable yarn repo' do
+  command 'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list'
+end
+
+execute 'update apt-get' do
+  command 'sudo apt update'
+end
+
 execute 'install yarn' do
-  command 'sudo apt-get install -y --force-yes yarn'
+  command 'sudo apt -y install yarn'
 end
